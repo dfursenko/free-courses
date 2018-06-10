47faeb27f4e6ff504355b358657e35f577fb4e30
Dmitry Fursenko <dfursenko@gmail.com>
2018-06-10 20:04:06 +0700

    Ruby. Задание 2. Вариант 1.

--- task2.rb
+++ task2.rb
@@ -1,4 +1,4 @@
 s = ARGV[0]
 prefix = ''
-(0...s.length).map { |i| prefix = s[0..i] if s[1, s.length].scan(s[0..i]).size > 1 }
+(0...s.length).map { |i| prefix = s[0..i] if s[1, s.length].include? s[0..i] }
 puts prefix
